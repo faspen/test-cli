d8d046360f34371abc01d44f2add5af93a220aa7
faspen <2335@holbertonschool.com>
2021-05-27 02:03:44 +0000

    added stuff

--- console/__main__.py
+++ console/__main__.py
@@ -2,8 +2,16 @@ import sys
 from .classmodule import MyClass
 from .funcmodule import my_function
 
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+
 
 def main():
+    if "--about" in opts:
+        print("Print stuff, I guess")
+        return
+    if "--tell-a-joke" in opts:
+        print("Why did the chicken cross the road?\nTo get to the other side!")
+        return
     print('in main')
     args = sys.argv[1:]
     print('count of args :: {}'.format(len(args)))
